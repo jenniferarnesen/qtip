44b3628effca5a4e8a63b841ba602a474da23ad0
Jennifer Arnesen <jennifer.arnesen@fronter.com>
2013-12-05 18:28:28 +0100

    stuff

--- js/qtip.js
+++ js/qtip.js
@@ -1,32 +1,34 @@
 $( document ).ready(function() {
 
 	var questionData =
-	{
-		max: 4,
-		min: 0,
-		decrease: 1
-	},
-
+		{
+			max: 4,
+			min: 0,
+			decrease: 1
+		},
 		totalScore = questionData.max,
 		reduceScore = function(){
-			totalScore = totalScore - questionData.decrease
+			totalScore = totalScore - questionData.decrease;
+			setMaxScoreText();
+		},
+		setMaxScoreText = function() {
+			$(".score").html("Maximum possible score: " + totalScore);
 		};
+
+	setMaxScoreText();
 
 	$(".tip-button").click(function() {
 		if ($(".tip.one").css('display') == 'none') {
 			$(".tip.one").show();
 			reduceScore();
-			console.log(totalScore);
 		}
 		else if ($(".tip.two").css('display') == 'none') {
 			$(".tip.two").show();
 			reduceScore();
-			console.log(totalScore);
   		}
   		else if ($(".tip.three").css('display') == 'none') {
   			$(".tip.three").show();
   			reduceScore();
-  			console.log(totalScore);
   		}
 	});
 
