90c6ed8476b19c5ecf2cc197628f99494e467c2e
Luis Cabral <lcabral37@gmail.com>
2013-12-06 10:50:59 +0100

    moved files around to have all the angualr app in the qtip.js file
    Added an angular player

--- js/qtip.js
+++ js/qtip.js
@@ -1,35 +1,66 @@
-$( document ).ready(function() {
 
-	var questionData =
-		{
-			max: 4,
-			min: 0,
-			decrease: 1
-		},
-		totalScore = questionData.max,
-		reduceScore = function(){
-			totalScore = totalScore - questionData.decrease;
-			setMaxScoreText();
-		},
-		setMaxScoreText = function() {
-			$(".score").html("Maximum possible score: " + totalScore);
-		};
+//use 'strict';
+angular.module('tipApp', ['elasticjs.service', 'ngSanitize'])
+    .controller('tipCtrl', function ($scope, ejsResource) {
+        var ejs = ejsResource('http://localhost:9200'),
+            ejsIndex = "qtip",
+            ejsTypes = "tip",
+            request = ejs.Request()
+                .indices(ejsIndex)
+                .types(ejsTypes);
 
-	setMaxScoreText();
+        $scope.data = {
+            "qid" : 0,
+            "decrement" : 0,
+            "limit" : 0,
+            "tips" : [],
+            "viewed" : []
+        };
 
-	$(".tip-button").click(function() {
-		if ($(".tip.one").css('display') == 'none') {
-			$(".tip.one").show();
-			reduceScore();
-		}
-		else if ($(".tip.two").css('display') == 'none') {
-			$(".tip.two").show();
-			reduceScore();
-  		}
-  		else if ($(".tip.three").css('display') == 'none') {
-  			$(".tip.three").show();
-  			reduceScore();
-  		}
-	});
+        $scope.loadTip = function(id) {
+            var termQuery = ejs.TermQuery("qid", id);
 
-});
+            $scope.data.qid = id;
+
+            request.query(termQuery).doSearch()
+                .then(
+                    function (data) {
+                        if (data.hits.hits[0]) {
+                            $scope.data = data.hits.hits[0]._source;
+                        }
+                        $scope.data.viewed = [];
+                    }
+            );
+        }
+
+        $scope.viewTip = function() {
+            console.log($scope.data);
+            if ($scope.data.tips.length) {
+                var tmp = $scope.data.tips.shift();
+                console.log(tmp);
+                $scope.data.viewed.push(tmp);
+            }
+        }
+
+        $scope.saveTip = function() {
+            console.log($scope.data);
+            ejs.Document(ejsIndex, ejsTypes, $scope.data.qid)
+            .source($scope.data)
+            .doIndex();
+        }
+
+        $scope.addTip = function() {
+            $scope.data.tips.push($scope.tipText);
+            $scope.tipText = '';
+            $scope.saveTip();
+        };
+
+        $scope.removeTip = function(index) {
+            $scope.data.tips.splice(index, 1);
+            $scope.saveTip();
+        };
+    }
+);
+
+
+
