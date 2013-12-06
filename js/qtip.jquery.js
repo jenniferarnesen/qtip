da24391760289e836174ba5cd0c7c31dc486f8aa
Luis Cabral <lcabral37@gmail.com>
2013-12-06 10:49:56 +0100

    moved files around...
    keeped the jquery version in its own .jquery files

---
+++ js/qtip.jquery.js
@@ -0,0 +1,36 @@
+
+$( document ).ready(function() {
+
+   var questionData =
+       {
+           max: 4,
+           min: 0,
+           decrease: 1
+       },
+       totalScore = questionData.max,
+       reduceScore = function(){
+           totalScore = totalScore - questionData.decrease;
+           setMaxScoreText();
+       },
+       setMaxScoreText = function() {
+           $(".score").html("Maximum possible score: " + totalScore);
+       };
+
+   setMaxScoreText();
+
+   $(".tip-button").click(function() {
+       if ($(".tip.one").css('display') == 'none') {
+           $(".tip.one").show();
+          reduceScore();
+       }
+       else if ($(".tip.two").css('display') == 'none') {
+           $(".tip.two").show();
+           reduceScore();
+       }
+       else if ($(".tip.three").css('display') == 'none') {
+           $(".tip.three").show();
+          reduceScore();
+       }
+   });
+
+});
